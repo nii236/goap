594c5f3c54a82bb0210ce924cbd29419bc8cf284
John Nguyen <jtnguyen236@gmail.com>
2018-12-15 23:38:19 +0800

    adds serve object handler

--- main.go
+++ main.go
@@ -21,10 +21,11 @@ func (c *Clock) Now() time.Time {
 func main() {
 	maxDeliveryDepth := 1
 	maxForwardingDepth := 1
-
+	clock := &Clock{}
+	app := &CombinedApplication{}
 	p := pub.NewPubber(
-		&Clock{},
-		&CombinedApplication{},
+		clock,
+		app,
 		&SocialCallbacker{},
 		&FederateCallbacker{},
 		&Deliverer{},
@@ -34,39 +35,63 @@ func main() {
 		maxForwardingDepth,
 	)
 
+	serveObjectHandler := pub.ServeActivityPubObject(app, clock)
 	r := chi.NewRouter()
-	c := &Controller{p}
+	c := &Controller{p, serveObjectHandler}
 	r.Get("/get_inbox", c.GetInbox)
 	r.Get("/get_outbox", c.GetOutbox)
 	r.Get("/post_inbox", c.PostInbox)
 	r.Get("/post_outbox", c.PostOutbox)
+	r.Get("/serve_object", c.ServeObject)
 
 	fmt.Println("Running server on :8080")
 	http.ListenAndServe(":8080", r)
 }
 
 type Controller struct {
-	p pub.Pubber
+	p                  pub.Pubber
+	serveObjectHandler pub.HandlerFunc
 }
 
+func (c *Controller) ServeObject(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	result, err := c.serveObjectHandler(ctx, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	fmt.Println(result, err)
+
+}
 func (c *Controller) GetInbox(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	result, err := c.p.GetInbox(ctx, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 	fmt.Println(result, err)
 }
 func (c *Controller) GetOutbox(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	result, err := c.p.GetOutbox(ctx, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 	fmt.Println(result, err)
 
 }
 func (c *Controller) PostInbox(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	result, err := c.p.PostInbox(ctx, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 	fmt.Println(result, err)
 }
 func (c *Controller) PostOutbox(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	result, err := c.p.PostOutbox(ctx, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 	fmt.Println(result, err)
 }
